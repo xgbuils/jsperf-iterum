0cee64abf7398d25441e2bd17441a90c45ccb11f
Xavier Garcia Buils <xgbuils@gmail.com>
2017-03-25 22:47:17 +0100

    add iterum@1.0.8

---
+++ iterum/1.0.8/bundle.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack')
+const version = '1.0.8'
+const underscoreVersion = version.split('.').join('_')
+
+const config = {
+    entry: ['./index.js'],
+    output: {
+        path: __dirname,
+        filename: 'cdn.js',
+        library: 'Iterum' + '_' + underscoreVersion,
+        libraryTarget: 'umd'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['env']
+            }
+        }]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: { warnings: false }
+        })
+    ]
+}
+
+webpack(config, function(err, stats) {
+    console.log(stats.toString({
+        colors: true
+    }))
+});
